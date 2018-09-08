036766e5e1a277cf1334a5108c2f3e819beb6595
xiaozhikang <xiaozhikang0916@gmail.com>
2018-09-08 22:40:03 +0800

    fix menu

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ function createWindow() {
     win = new BrowserWindow({ width: 300, height: 600 })
     win.loadFile('index.html')
     buildMenu()
-    win.webContents.openDevTools()
     win.on('closed', () => {
         if (setting) {
             setting.close()
@@ -17,11 +16,25 @@ function createWindow() {
 }
 
 function buildMenu() {
-    var template = []
+    var template = [{
+        label: "Edit",
+        submenu: [
+            { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+            { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+            { type: "separator" },
+            { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+            { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+            { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+            { label: "Select All", accelerator: "CmdOrCtrl+A", selector: "selectAll:" }
+        ]}]
     if (process.platform === "darwin") {
         template.unshift({
             label: app.getName(),
             submenu: [
+                {
+                    label: "打开开发者工具",
+                    click: showDevTool
+                },
                 {
                     label: "设置",
                     click: showSetting
@@ -36,6 +49,10 @@ function buildMenu() {
         template.unshift({
             label: "菜单",
             submenu: [
+                {
+                    label: "打开开发者工具",
+                    click: showDevTool
+                },
                 {
                     label: "设置",
                     click: showSetting
@@ -52,13 +69,16 @@ function buildMenu() {
 function showSetting(menuItem, browserWindow, event) {
     setting = new BrowserWindow({ width: 300, height: 300 })
     setting.loadFile('settings.html')
-    setting.webContents.openDevTools()
     setting.on('closed', () => {
         setting = null
         if (win) {
             win.reload()
         }
     })
+}
+
+function showDevTool(menuItem, browserWindow, event) {
+    browserWindow.webContents.openDevTools()
 }
 
 
