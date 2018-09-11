1c483abbd4d9e8252a0dd5e5327c0b27cf671eac
xiaozhikang <xiaozhikang0916@gmail.com>
2018-09-11 10:23:48 +0800

    resolve md into mod info page

--- scripts/modinfo.js
+++ scripts/modinfo.js
@@ -1,3 +1,5 @@
+const marked = require('marked')
+
 function buildInfoPage(modName, template) {
     var icon = template.querySelector(".icon")
     var title = template.querySelector(".title")
@@ -6,12 +8,21 @@ function buildInfoPage(modName, template) {
     var button = template.querySelector(".modbutton")
     if (fs.existsSync(path.join(getModPath(), ".modinfo", modName, "info.json"))) {
         var modInfo = JSON.parse(fs.readFileSync(path.join(getModPath(), ".modinfo", modName, "info.json"), 'utf8'))
+        icon.style.display = "block"
         icon.src = path.join(getModPath(), ".modinfo", modName, "icon.png")
         title.textContent = modInfo.name
         author.textContent = modInfo.author
         tags.textContent = modInfo.tags.join(',')
+        template.querySelector(".readme_section").innerHTML = marked.parse(
+            fs.readFileSync(path.join(getModPath(), ".modinfo", modName, "readme.md"), 'utf8'),
+            {
+                baseUrl: path.join(getModPath(), ".modinfo", modName, './'),
+                gfm: true
+            }
+        )
     } else {
         title.textContent = modName
+        icon.style.display = "none"
     }
     generateButton(button, modName, false)
     template.firstElementChild.id = `mod_info_${modName}`
