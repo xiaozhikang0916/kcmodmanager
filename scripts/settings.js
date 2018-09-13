c9d79d33ca9b4d4b7d89606e1aaa80f5e01f5628
xiaozhikang <xiaozhikang0916@gmail.com>
2018-09-13 19:47:36 +0800

    add select dir button to setting page

--- scripts/settings.js
+++ scripts/settings.js
@@ -2,6 +2,7 @@ var modPath = document.getElementById('mod_path_edit')
 var outputPath = document.getElementById('output_path_edit')
 var nameFormat = document.getElementById('name_fmt_edit')
 const { ipcRenderer } = require('electron')
+const { dialog } = require('electron').remote
 
 const config = new Config(ipcRenderer.sendSync('get-setting'))
 
@@ -20,18 +21,32 @@ if (config.getNameFormat()) {
     config.setNameFormat(nameFormat.value)
 }
 
-modPath.oninput = function(ev) {
+modPath.oninput = function (ev) {
     config.setModPath(modPath.value)
 }
 
-outputPath.oninput = function(ev) {
+outputPath.oninput = function (ev) {
     config.setExtractPath(outputPath.value)
-} 
+}
 
-nameFormat.oninput = function(ev) {
+nameFormat.oninput = function (ev) {
     config.setNameFormat(nameFormat.value)
 }
 
-window.onbeforeunload = function() {
+window.onbeforeunload = function () {
     ipcRenderer.sendSync("save-setting", config)
 }
+
+document.getElementById('mod_path_edit_button').addEventListener('click', (e, ev) => {
+    dialog.showOpenDialog({ properties: ['openDirectory'], multiSelections: false }, (filePaths) => {
+        modPath.value = filePaths[0]
+        config.setModPath(modPath.value)
+    })
+})
+
+document.getElementById('output_path_edit_button').addEventListener('click', (e, ev) => {
+    dialog.showOpenDialog({ properties: ['openDirectory'], multiSelections: false }, (filePaths) => {
+        outputPath.value = filePaths[0]
+        config.setExtractPath(outputPath.value)
+    })
+})
